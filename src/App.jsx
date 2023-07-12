9a984220d49dc6a2f0fe62ee81a813ebc57cd517
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-07-12 21:55:10 +0600

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-//import Headers from "./component/Headers";
-//import ContactForm from "./component/contactForm";
-//import Footer from "./component/footer";
-//import Hero from "./component/hero";
-
-function App() {
-  let marks =77;
+const App = () => {
   return (
     <div>
-
-{(()=>{
-
-if(marks>80 && marks<100){
-  return <h1>A+</h1>
-}
-if(marks>70 && marks<80){
-  return <h1>B+</h1>
-}
-if(marks>60 && marks<70){
-  return <h1>C+</h1>
-}
-
-})()}
-
-
+      <h1>fghfgh</h1>
     </div>
-    
   );
-}
+};
 
 export default App;
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,9 @@
 const Hero = () => {
   return (
-    <>
-      <div>
-        <h1>hero</h1>
-        <img src="https://placehold.co/600x400/000000/FFF"></img>
-      </div>
-    </>
+    <div>
+      <h1>hero</h1>
+      <button onClick={props.childBtnClick}>submit</button>
+    </div>
   );
 };
 
