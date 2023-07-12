a6456ea4c25e4d212ed893d73579c5055fabcf32
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-07-12 16:41:25 +0600

    .

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import reactLogo from "./assets/react.svg";
+// eslint-disable-next-line no-unused-vars
+import viteLogo from "/vite.svg";
+import "./App.css";
+// eslint-disable-next-line no-unused-vars
+import demo from "./demo";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>fsdfsdf</h1>
+      </div>
+      <demo />
+      <h1>my app react</h1>
+    </>
+  );
+}
+
+export default App;
